2139aff92b8604877e29e53edf314a31528342e2
Eder Martinez <edermtz.04@gmail.com>
2023-01-08 20:52:28 -0600

    Stage 2

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.kt
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.kt
@@ -1,7 +1,18 @@
 package tictactoe
 
 fun main() {
-    println("X O X")
-    println("O X O")
-    println("X O O")
+    val input: String = readln()
+    println("-".repeat(9))
+    print("| ")
+    print("${input[0]} ${input[1]} ${input[2]}")
+    print(" |")
+    println()
+    print("| ")
+    print("${input[3]} ${input[4]} ${input[5]}")
+    print(" |")
+    println()
+    print("| ")
+    print("${input[6]} ${input[7]} ${input[8]}")
+    println(" |")
+    println("-".repeat(9))
 }
